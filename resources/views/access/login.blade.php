ef5c83f1a692b79a309b352638265bd11cad3184
janfernandezg <janfernandezg@gmail.com>
2018-05-21 03:59:14 -0400

    3° commit
    Cambio fondo y colores de login

--- resources/views/access/login.blade.php
+++ resources/views/access/login.blade.php
@@ -24,11 +24,10 @@
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="brainy-bg-style" style="display:flex;">
+<body class="BEGAMES-bg-style">
 <div class="login-boxer">
     <div class="text-center">
         <img src="#" alt="BEGAMES" width="70%">
-        <p class="login-boxer-p">BEGAMES</p>
     </div>
     <div class="login-box">
         <div class="row">
