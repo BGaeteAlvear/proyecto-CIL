b7109efc40b7346308096fdf1a0478d29195aaed
Brian Gaete Alvear <b.gaete.digilab@gmail.com>
2018-05-21 14:53:42 -0400

    correccion de colores template begames

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,20 +1,12 @@
 <header class="main-header">
     <!-- Logo -->
-    {{--<a href="/index2.html" class="logo">--}}
-        {{--<!-- mini logo for sidebar mini 50x50 pixels -->--}}
-        {{--<span class="logo-mini"></span>--}}
-        {{--<!-- logo for regular state and mobile devices -->--}}
-        {{--<span class="logo-lg">Brainy Educator</span>--}}
-
-
-    {{--</a>--}}
-
-    {{--<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">--}}
-        {{--<span class="sr-only">Toggle navigation</span>--}}
-        {{--<span class="icon-bar"></span>--}}
-        {{--<span class="icon-bar"></span>--}}
-        {{--<span class="icon-bar"></span>--}}
-    {{--</a>--}}
+    <a href="{{ route('dashboard') }}" class="logo">
+          <!-- mini logo for sidebar mini 50x50 pixels -->
+          <span class="logo-mini"><b>B</b>Ga</span>
+          <!-- logo for regular state and mobile devices -->
+          <span class="logo-lg"><b>BE</b>Games</span>
+        </a>
+    </a>
     <!-- Header Navbar: style can be found in header.less -->
     @include('layout.navbar')
 </header>
