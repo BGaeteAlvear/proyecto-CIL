6639c75029eed12710a1187a0ba2137d9f1f1a11
Unknown <mnferruzola@gmail.com>
2018-12-11 00:47:49 -0300

    Modificacion Titulo Header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,7 +4,7 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>C</b>IL</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Colegio</b>Infantes</span>
+          <span class="logo-lg"><b>Colegio</b> Infantes</span>
         </a>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
